8e59c91e4dc55e6fb64ccbed056dd53fbeaf680d
Ananya Rajput <ananyarajput697@gmail.com>
2026-03-08 10:59:08 +0530

    Restructured project and added sorting algorithms

---
+++ sorting/shell_sort.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void shellSort(vector<int>& arr) {
+    int n = arr.size();
+
+    for(int gap = n/2; gap > 0; gap /= 2) {
+
+        for(int i = gap; i < n; i++) {
+
+            int temp = arr[i];
+            int j;
+
+            for(j = i; j >= gap && arr[j-gap] > temp; j -= gap) {
+                arr[j] = arr[j-gap];
+            }
+
+            arr[j] = temp;
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    shellSort(arr);
+
+    for(int i = 0; i < n; i++) {
+        if(i) cout << " ";
+        cout << arr[i];
+    }
+
+    return 0;
+}
